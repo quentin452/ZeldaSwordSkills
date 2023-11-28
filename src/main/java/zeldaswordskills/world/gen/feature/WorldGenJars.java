eff3e7dbb1d59753460b53a63ef94f158598ac95
QUENTIN453 <quentinlamauve@gmail.com>
2023-11-28 06:22:53 +0100

    Should fix #1

--- src/main/java/zeldaswordskills/world/gen/feature/WorldGenJars.java
+++ src/main/java/zeldaswordskills/world/gen/feature/WorldGenJars.java
@@ -1,16 +1,16 @@
 /**
  * Copyright (C) <2015> <coolAlias>
- * 
+ *
  * This file is part of coolAlias' Zelda Sword Skills Minecraft Mod; as such,
  * you can redistribute it and/or modify it under the terms of the GNU
  * General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -51,15 +51,15 @@ public class WorldGenJars extends WorldGenerator {
     }
 
     private boolean canPlaceBlockAt(World world, int i, int j, int k, boolean isUnderground) {
+        if (world.getBlock(i, j, k).getMaterial() == Material.water) {
+            return false;
+        }
         return world.isAirBlock(i, j, k) || (!isUnderground && Config.genJarsInWater()
-            && world.getBlock(i, j, k)
-                .getMaterial() == Material.water
             && !world.canBlockFreeze(i, j, k, false));
     }
-
     /**
      * Attempts to generate a single jar cluster
-     * 
+     *
      * @param jarsPerCluster max number of jars to generate in this cluster
      */
     public void doJarGen(World world, Random rand, int chunkX, int chunkZ, int jarsPerCluster, boolean isUnderground) {
